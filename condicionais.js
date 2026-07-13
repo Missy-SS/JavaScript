57a3399475737c9601cd712d1821dac2236fc5df
Melissa Santos <melissilva123@gmail.com>
2026-07-13 16:18:12 -0300

    Criação e adição do condicionais.js

---
+++ condicionais.js
@@ -0,0 +1,25 @@
+const idade = 17;
+
+
+//utilizando o if
+if (idade>=18) {
+    console.log("É maior de idade")
+
+} else {
+    console.log("É menor de idade")
+}
+
+//múltiplas condições
+
+const notaDoAluno = 8;
+
+if (notaDoAluno >= 9) {
+    console.log("Nota excelente!");
+} else if (notaDoAluno => 7) {
+    console.log("Nota boa!");
+} else if (notaDoAluno >= 4) {
+    console.log("Nota mediana!");
+} else {
+    console.log("Nota ruim");
+}
+

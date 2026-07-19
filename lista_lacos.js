7415fc2ca156efac238ac8e0a39d6cb914d83d9e
Melissa Santos <melissilva123@gmail.com>
2026-07-18 23:10:21 -0300

    Add exercises for sum and multiplication table

--- lista_lacos.js
+++ lista_lacos.js
@@ -5,3 +5,18 @@ for(let contador = 0; contador <= 10; contador++){
 }
 
 console.log("Exercício 1 feito");
+
+let soma = 0;
+for (let i = 1; i <= 100; i++) {
+    soma += i;
+    
+}
+console.log("Soma:",soma)
+
+console.log("Exercício 2 feito");
+
+let numero = 2;
+
+for(let i = 0; i <= 10; i++){
+    console.log(numero + "x" + i + "=" + (numero * i));
+}

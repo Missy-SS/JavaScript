125178522f7402827b343dc2857067cb078533ad
Melissa Santos <melissilva123@gmail.com>
2026-07-31 12:20:40 -0300

    Add function to calculate double of a number

--- funcoes.js
+++ funcoes.js
@@ -5,3 +5,10 @@ function saudacao(nome) {
 saudacao("Mel")
 saudacao("Vanessa")
 saudacao("Day")
+
+function calcularDobro (numero){
+    return numero * 2
+}
+
+const numeroDobrado = calcularDobro(4)
+console.log("O dobro de 4 é:", numeroDobrado)

40a15d865999135be1b79f16880152ddc8932d07
Melissa Santos <melissilva123@gmail.com>
2026-08-20 17:46:15 -0300

    Funcionamento da função setTimeout

---
+++ funcoes_globais.js
@@ -0,0 +1,9 @@
+//setTimeout
+//Executa um bloco de código, uma função, após um determinado período de tempo
+
+function saudacao () {
+    console.log("E ai, beleza?")
+}
+
+setTimeout(saudacao, 2000);
+//unidade de medida em milissegundos

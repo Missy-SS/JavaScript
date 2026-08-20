d84a42cc881a83cdd55a50829db64a3c9bdef1cb
Melissa Santos <melissilva123@gmail.com>
2026-08-20 17:55:39 -0300

    setInterval e sua funcionalidade em JS
    Add setInterval to log elapsed time every second.

--- funcoes_globais.js
+++ funcoes_globais.js
@@ -7,3 +7,18 @@ function saudacao () {
 
 setTimeout(saudacao, 2000);
 //unidade de medida em milissegundos
+
+//setInterval
+// Dois argumentos:
+// Primeiro: o que queremos fazer; uma função
+// Segundo: de quanto em quanto tempo; intervalo de tempo
+
+let contador = 0;
+
+const id = setInterval(() => {
+    contador ++;
+    console.log("Tempo decorrido (em segundos) = ", contador)
+    if (contador == 10) {
+        clearInterval(id);
+    }
+}, 1000);

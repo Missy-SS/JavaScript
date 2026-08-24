6862b5ebb3e08fb1ca3d0f82b70d5125aa1511fa
Melissa Santos <melissilva123@gmail.com>
2026-08-23 21:13:16 -0300

    Funcionamento de um Array em JS

---
+++ Aula09/Arrays.js
@@ -0,0 +1,11 @@
+const frutas = ["Uva","Banana","Kiwi","Maçã","Morango"];
+
+// Acessando elementos em Array
+
+console.log(frutas);
+
+console.log("Primeira fruta: ", frutas[0]);
+
+console.log("Última fruta: ", frutas[4]);
+
+console.log("Total de frutas: ", frutas.length);

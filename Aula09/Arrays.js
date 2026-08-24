093651fdd2e0761a454ac71f4c0e75efcef4ce74
Melissa Santos <melissilva123@gmail.com>
2026-08-23 21:24:20 -0300

    Remoção de arrays e como percorrê-los

--- Aula09/Arrays.js
+++ Aula09/Arrays.js
@@ -21,3 +21,19 @@ console.log("Total de frutas depois de adicionar melancia", frutas.length);
 //A última posição é o total de itens menos um
 
 console.log("Últime fruta: ", frutas[frutas.length - 1]);
+
+//Listas e remoção
+
+//Passamos o índice, a posição e quantos queremos remover
+frutas.splice(2,1)
+
+console.log("Depois de remover", frutas);
+
+//Percorrendo Arrays
+
+//Utilizando for
+
+for(let i = 0; i < frutas.length; i++){
+    console.log("Índice: ", i)
+    console.log(frutas[i])
+}

221afd52ffbdef39414961b3c99a7ed14885d747
Melissa Santos <melissilva123@gmail.com>
2026-08-23 21:18:50 -0300

    Funcionalidades de contagem, adição e acesso

--- Aula09/Arrays.js
+++ Aula09/Arrays.js
@@ -8,4 +8,16 @@ console.log("Primeira fruta: ", frutas[0]);
 
 console.log("Última fruta: ", frutas[4]);
 
+//Contando elementos
+
 console.log("Total de frutas: ", frutas.length);
+
+//Adicionando elementos e acessando dinamicamente
+
+frutas.push("Melancia");
+
+console.log("Total de frutas depois de adicionar melancia", frutas.length);
+
+//A última posição é o total de itens menos um
+
+console.log("Últime fruta: ", frutas[frutas.length - 1]);

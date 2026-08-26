d73530f828002517c9a8ba79660b1530fef06416
Melissa Santos <melissilva123@gmail.com>
2026-08-26 15:25:16 -0300

    Utilizando o método Filter em arrays
    Implement filter method to separate even and odd numbers from an array.

---
+++ Aula09/metodos_arrays.js
@@ -0,0 +1,26 @@
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+//método Filter
+//Filtrar elementos com base em uma condição específica
+//Acessa o número e retorna uma condição verdadeira ou falsa
+//Filtra apenas quando é verdadeira
+
+//Filtrando números pares
+
+const numerosPares = numeros.filter((numero) => {
+    return numero % 2 == 0;
+});
+
+//Armazenar o resultado em um variável
+
+console.log("Todos os números: ", numeros);
+console.log("Todos os números pares: ", numerosPares);
+
+//Filtrando números ímpares
+
+const numerosImpares = numeros.filter((numero) => {
+    return numero % 2 != 0;
+});
+
+console.log("Todos os números ímpares: ", numerosImpares);
+

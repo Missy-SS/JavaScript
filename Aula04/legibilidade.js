7c1088a834897510b8968b92ef04791fc1c528af
Melissa Santos <melissilva123@gmail.com>
2026-09-01 11:16:14 -0300

    Criação da legibilidade.js

---
+++ Aula04/legibilidade.js
@@ -0,0 +1,14 @@
+let altura = 7;
+let largura = 10;
+let area = altura * largura;
+console.log(area)
+
+let cliques = 0;
+cliques ++;
+cliques ++;
+cliques ++;
+console.log(cliques);
+
+let contador = 5;
+console.log(contador++);
+console.log(contador);

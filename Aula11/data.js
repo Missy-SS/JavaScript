d41b5fc4c44dfdc3d0eebf98ff2cb42ab23a5f3e
Melissa Santos <melissilva123@gmail.com>
2026-09-01 12:47:04 -0300

    Introduzindo a função de Data

---
+++ Aula11/data.js
@@ -0,0 +1,16 @@
+const agora = new Date()
+
+console.log(agora)
+//Extraindo informações específicas da data
+
+//Obter o ano atual
+console.log("Ano:", agora.getFullYear())
+
+//Obter o mês
+//O mês 0 representa janeiro (0-11)
+
+console.log("Mês 0-11", agora.getMonth())
+
+//Obter o dia do mês
+
+console.log("Dia do mês:", agora.getDate())
